212c394545b9e2183e08266c6cd4d9bd77354bc7
Vadim Kravchenko <pnevmat001@gmail.com>
2020-12-08 19:26:28 +0200

    Fixes hw 8 js

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -5,6 +5,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
+    dataindex: '0',
   },
   {
     preview:
@@ -12,6 +13,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
+    dataindex: '1',
   },
   {
     preview:
@@ -19,6 +21,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
+    dataindex: '2',
   },
   {
     preview:
@@ -26,6 +29,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
     description: 'Flower Blooms',
+    dataindex: '3',
   },
   {
     preview:
@@ -33,6 +37,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
     description: 'Alpine Mountains',
+    dataindex: '4',
   },
   {
     preview:
@@ -40,6 +45,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
     description: 'Mountain Lake Sailing',
+    dataindex: '5',
   },
   {
     preview:
@@ -47,6 +53,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
     description: 'Alpine Spring Meadows',
+    dataindex: '6',
   },
   {
     preview:
@@ -54,6 +61,7 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
+    dataindex: '7',
   },
   {
     preview:
@@ -61,5 +69,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
+    dataindex: '8',
   },
 ];
--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ const createImgListItem = (arr) => {
     listItemLinkImgRef.src = arr.preview;
     listItemLinkImgRef.alt = arr.description;
     listItemLinkImgRef.dataset.source = arr.original;
+    listItemLinkImgRef.dataset.index = arr.dataindex;
 
     listItemRef.appendChild(listItemLinkRef);
     listItemLinkRef.appendChild(listItemLinkImgRef);
@@ -22,11 +23,72 @@ const createImgListItem = (arr) => {
     return listItemRef;
 };
 
+// Вызываем функцию рендера перебором массива
+
 const imgListArray = galleryItems.map(galleryItems => createImgListItem(galleryItems));
+
+// распыляем массив созданной размет в DOM ul
 
 containerListRef.append(...imgListArray);
 
+// ссылки на кнопки открытия и закрытия модалки
+
+const modalWindowRef = document.querySelector('.js-lightbox');
+const closeModalBtnRef = document.querySelector('[data-action=close-lightbox]');
+
+// ссылка на тег img в разметке модалки
+
+const modalImageRef = document.querySelector('.lightbox__image');
+
+// слушатель события на открытие модалки
+
 containerListRef.addEventListener('click', () => {
-    const currentImgClick = Event.currentTarget.dataset.source;
-    console.log(currentImgClick);
+    modalWindowRef.classList.add('is-open');
+    const currentImgClick = event.target.dataset.source;
+    modalImageRef.src = currentImgClick;
+    event.preventDefault();
+});
+
+// слушатель событи на закрытие модалки по клику на кнопку
+
+closeModalBtnRef.addEventListener('click', () => {
+    modalWindowRef.classList.remove('is-open');
+    modalImageRef.src = '';
+});
+
+// ссылка на div.lightbox__overlay
+
+const lightBoxOverlay = document.querySelector('.lightbox__overlay');
+
+// слушатель событи на закрытие модалки по клику на overlay
+
+lightBoxOverlay.addEventListener('click', () => {
+    modalWindowRef.classList.remove('is-open');
+    modalImageRef.src = '';
+});
+
+// слушатель события на клавишу esc
+
+window.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+        modalWindowRef.classList.remove('is-open');
+        modalImageRef.src = '';
+    } else {}
+});
+
+let startIndex = '';
+
+window.addEventListener('keydown', event => {
+    // console.log('Event is on');
+    // console.dir(modalWindowRef.classList);
+    // debugger;
+    if (modalWindowRef.classList[2] === 'is-open') {
+        // console.dir(event.target);
+        if (event.code === 'ArrowLeft') {
+            startIndex = event.target.childNodes[0].dataset.index;
+            startIndex = Number(startIndex - 1);
+            modalImageRef.src = galleryItems[startIndex].original;
+            console.log(modalImageRef.src);
+        }
+    }
 });
