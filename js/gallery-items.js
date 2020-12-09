6be85a3ae7eb92d07fde41a6fcc5722e66f96c7b
Vadim Kravchenko <pnevmat001@gmail.com>
2020-12-09 16:37:49 +0200

    Fixes code for last task of module 8

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -5,7 +5,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
-    dataindex: '0',
   },
   {
     preview:
@@ -13,7 +12,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
-    dataindex: '1',
   },
   {
     preview:
@@ -21,7 +19,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
-    dataindex: '2',
   },
   {
     preview:
@@ -29,7 +26,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
     description: 'Flower Blooms',
-    dataindex: '3',
   },
   {
     preview:
@@ -37,7 +33,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
     description: 'Alpine Mountains',
-    dataindex: '4',
   },
   {
     preview:
@@ -45,7 +40,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
     description: 'Mountain Lake Sailing',
-    dataindex: '5',
   },
   {
     preview:
@@ -53,7 +47,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
     description: 'Alpine Spring Meadows',
-    dataindex: '6',
   },
   {
     preview:
@@ -61,7 +54,6 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
-    dataindex: '7',
   },
   {
     preview:
@@ -69,6 +61,5 @@ export default [
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
-    dataindex: '8',
   },
 ];
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const containerListRef = document.querySelector('.js-gallery');
 
 // функция рендера разметки
 
-const createImgListItem = (arr) => {
+const createImgListItem = (arr, index) => {
     const listItemRef = document.createElement('li');
     listItemRef.classList.add('gallery__item');
     const listItemLinkRef = document.createElement('a');
@@ -15,7 +15,7 @@ const createImgListItem = (arr) => {
     listItemLinkImgRef.src = arr.preview;
     listItemLinkImgRef.alt = arr.description;
     listItemLinkImgRef.dataset.source = arr.original;
-    listItemLinkImgRef.dataset.index = arr.dataindex;
+    listItemLinkImgRef.dataset.index = index;
 
     listItemRef.appendChild(listItemLinkRef);
     listItemLinkRef.appendChild(listItemLinkImgRef);
@@ -25,7 +25,7 @@ const createImgListItem = (arr) => {
 
 // Вызываем функцию рендера перебором массива
 
-const imgListArray = galleryItems.map(galleryItems => createImgListItem(galleryItems));
+const imgListArray = galleryItems.map((item, index) => createImgListItem(item, index));
 
 // распыляем массив созданной размет в DOM ul
 
@@ -45,7 +45,10 @@ const modalImageRef = document.querySelector('.lightbox__image');
 containerListRef.addEventListener('click', () => {
     modalWindowRef.classList.add('is-open');
     const currentImgClick = event.target.dataset.source;
+    const currentImgIndex = event.target.dataset.index;
     modalImageRef.src = currentImgClick;
+    modalImageRef.dataset.index = currentImgIndex;
+
     event.preventDefault();
 });
 
@@ -76,19 +79,36 @@ window.addEventListener('keydown', event => {
     } else {}
 });
 
-let startIndex = '';
+// слушатель события на клавиши вправо\лево
 
 window.addEventListener('keydown', event => {
-    // console.log('Event is on');
-    // console.dir(modalWindowRef.classList);
-    // debugger;
     if (modalWindowRef.classList[2] === 'is-open') {
-        // console.dir(event.target);
-        if (event.code === 'ArrowLeft') {
-            startIndex = event.target.childNodes[0].dataset.index;
-            startIndex = Number(startIndex - 1);
-            modalImageRef.src = galleryItems[startIndex].original;
-            console.log(modalImageRef.src);
+        if (event.code === 'ArrowRight') {
+            pressRight();
+        } else if (event.code === 'ArrowLeft') {
+            pressLeft();
         }
     }
 });
+
+// функция-обработчик события нажатия клавиши вправо
+
+function pressRight() {
+    const index = Number(modalImageRef.dataset.index);
+    if (index < galleryItems.length - 1) {
+        modalImageRef.src = galleryItems[index + 1].original;
+        modalImageRef.alt = galleryItems[index + 1].description;
+        modalImageRef.dataset.index = `${index + 1}`;
+    };
+};
+
+// функция-обработчик события нажатия клавиши влево
+
+function pressLeft() {
+    const index = Number(modalImageRef.dataset.index);
+    if (index > 0) {
+        modalImageRef.src = galleryItems[index - 1].original;
+        modalImageRef.alt = galleryItems[index - 1].description;
+        modalImageRef.dataset.index = `${index - 1}`;
+    };
+};
